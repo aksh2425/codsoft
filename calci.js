cb7f4639d4553a8297913dc290a220b23de1b66f
aksh2425 <129761322+aksh2425@users.noreply.github.com>
2023-09-05 21:48:02 +0530

    Update calci.js

--- calci.js
+++ calci.js
@@ -2,14 +2,10 @@
 function clearScreen() {
     document.getElementById("result").value = "";
 }
- 
-// This function display values
-function display(value) {
+ function display(value) {
     document.getElementById("result").value += value;
 }
- 
-// This function evaluates the expression and returns result
-function calculate() {
+ function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
